8f099b50128c44d25d289bc9129075db1a8d1025
cmchew <write_on@hotmail.com>
2020-01-06 15:30:52 -0500

    whatever-commit-message

---
+++ src/main/java/com/example/demo/L102Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L102Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L102Application.class, args);
+    }
+
+}
